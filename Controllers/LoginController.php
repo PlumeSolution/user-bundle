4ac32d4b7d8e90dedd2f2347d14c6e6f898d3e1d
GothShoot <romuald59161@gmail.com>
2020-03-10 16:09:39 +0100

    Fix Form rendering in login feature

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace PSUserBundle\Controllers;
 
+use PSUserBundle\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,49 +10,49 @@ use Symfony\Component\HttpFoundation\Response;
 
 abstract class LoginController extends AbstractController implements LoginInterface
 {
-    /**
-     * @inheritDoc
-     */
-    public function __invoke(Request $request): Response
-    {
-        if ($this->isGranted('IS_AUTHENTICATED_FULLY'))
-        {
-            return $this->redirectIfAlreadyLogged();
-        }
+	/**
+	 * @inheritDoc
+	 */
+	public function __invoke(Request $request): Response
+	{
+		if ($this->isGranted('IS_AUTHENTICATED_FULLY'))
+		{
+			return $this->redirectIfAlreadyLogged();
+		}
 
-        $form = $this->getLoginForm();
+		$form = $this->getLoginForm();
 
-        $form->handleRequest($request);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            return $this->redirectIfAlreadyLogged();
-        }
+		if ($form->isSubmitted() && $form->isValid())
+		{
+			return $this->redirectIfAlreadyLogged();
+		}
 
-        return $this->renderTemplate($form->createView());
-    }
+		return $this->renderTemplate($form);
+	}
 
-    /**
-     * @inheritDoc
-     */
-    protected function redirectIfAlreadyLogged(): Response
-    {
-        return $this->redirect('/');
-    }
+	/**
+	 * @inheritDoc
+	 */
+	protected function redirectIfAlreadyLogged(): Response
+	{
+		return $this->redirect('/');
+	}
 
-    /**
-     * @inheritDoc
-     */
-    protected function getLoginForm(): FormInterface
-    {
-        return $this->createForm('PSUserBundle\Form\LoginType');
-    }
+	/**
+	 * @inheritDoc
+	 */
+	protected function getLoginForm(): FormInterface
+	{
+		return $this->createForm(LoginType::class);
+	}
 
-    /**
-     * @inheritDoc
-     */
-    protected function renderTemplate(FormInterface $form): Response
-    {
-        return $this->render('@PSUser/Login/login.html.twig', ['form' => $form]);
-    }
+	/**
+	 * @inheritDoc
+	 */
+	protected function renderTemplate(FormInterface $form): Response
+	{
+		return $this->render('Login/login.html.twig', ['form' => $form->createView()]);
+	}
 }
