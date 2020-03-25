9f2413bd5663cdd913f560263f5fe38b77be599f
GothShoot <romuald.mocq@yahoo.com>
2020-03-25 08:54:36 +0100

    Fix equatable required function (overide needed for custom user)

---
+++ Fields/EqualTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PSUserBundle\Fields;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+trait EqualTrait
+{
+    public function serialize(): string
+    {
+        return serialize([
+            $this->id,
+            $this->nickname,
+            $this->password,
+            $this->salt
+        ]);
+    }
+
+    public function unserialize($serialized): void
+    {
+        [
+            $this->id,
+            $this->nickname,
+            $this->password,
+            $this->salt
+        ]
+
+            = unserialize($serialized, ['allowed_classes' => false]);
+    }
+
+    public function isEqualTo(UserInterface $user): bool
+    {
+        return $user->getUsername() === $this->getUsername();
+    }
+}
--- Models/NicknamePasswordUser.php
+++ Models/NicknamePasswordUser.php
@@ -3,61 +3,19 @@
 namespace PSUserBundle\Models;
 
 use PSUserBundle\Credentials\NicknamePasswordTrait;
+use PSUserBundle\Fields\EqualTrait;
 use PSUserBundle\Fields\RolesTrait;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class NicknamePasswordUser extends BaseUser
 {
     use NicknamePasswordTrait;
     use RolesTrait;
+    use EqualTrait;
 
     public function __construct(?string $nickname, ?string $password, ?string $salt)
     {
         $this->setNickname($nickname);
         $this->setPassword($password);
         $this->setSalt($salt);
-    }
-
-    /**
-     * String representation of object
-     * @link  https://php.net/manual/en/serializable.serialize.php
-     * @return string the string representation of the object or null
-     * @since 5.1.0
-     */
-    public function serialize()
-    {
-        // TODO: Implement serialize() method.
-    }
-
-    /**
-     * Constructs the object
-     * @link  https://php.net/manual/en/serializable.unserialize.php
-     *
-     * @param string $serialized <p>
-     *                           The string representation of the object.
-     *                           </p>
-     *
-     * @return void
-     * @since 5.1.0
-     */
-    public function unserialize($serialized)
-    {
-        // TODO: Implement unserialize() method.
-    }
-
-    /**
-     * The equality comparison should neither be done by referential equality
-     * nor by comparing identities (i.e. getId() === getId()).
-     *
-     * However, you do not need to compare every attribute, but only those that
-     * are relevant for assessing whether re-authentication is required.
-     *
-     * @param UserInterface $user
-     *
-     * @return void
-     */
-    public function isEqualTo(UserInterface $user)
-    {
-        // TODO: Implement isEqualTo() method.
     }
 }
