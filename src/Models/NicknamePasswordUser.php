bb8238d88d883fd35c371830567037dbcbb7ba2e
GothShoot <romuald.mocq@plumesolution.fr>
2021-12-28 15:59:16 +0100

    Add to password covered user the new interface for password hashing.

--- src/Models/NicknamePasswordUser.php
+++ src/Models/NicknamePasswordUser.php
@@ -5,8 +5,9 @@ namespace PlumeSolution\UserBundle\Models;
 use PlumeSolution\UserBundle\Credentials\NicknamePasswordTrait;
 use PlumeSolution\UserBundle\Fields\EqualTrait;
 use PlumeSolution\UserBundle\Fields\RolesTrait;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
-class NicknamePasswordUser extends BaseUser
+class NicknamePasswordUser extends BaseUser implements PasswordAuthenticatedUserInterface
 {
     use NicknamePasswordTrait;
     use RolesTrait;
